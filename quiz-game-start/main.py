d8eb94a42a648cae645286a075f57e5740e5d5c4
dhruvmalhotra <dhruvmalhotra2020@gmail.com>
2023-05-11 22:11:45 -0500

    Questions, continues to the next question

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+
 question_bank = []
 for question in question_data:
     question_text = question["text"]
@@ -9,4 +10,5 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
